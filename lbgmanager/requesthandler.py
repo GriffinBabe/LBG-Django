c16012fd12a9aa6af0b52af3065d1f8815efdd52
Darius Couchard <darius.couchard@hotmail.com>
2019-04-19 12:40:08 +0200

    Added get_model and get_authentication_key request handlers

--- lbgmanager/requesthandler.py
+++ lbgmanager/requesthandler.py
@@ -1,28 +1,79 @@
 from django.http import HttpResponse
-import json
-from lbgmanager.models import Member, Task, Event
+import json, hashlib, random, string
+from lbgmanager.models import *
+
+
+def generate_token(user):
+    """ Generates a authentication token and sets it to the user"""
+    while True:
+        random_token = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+        try:  # Checks if no user already has this random generated token (it's highly improbable)
+            Member.objects.get(auth_key=random_token)
+            continue
+        except Member.DoesNotExist:
+            break
+    user.auth_key = random_token
+    user.save()
+    return random_token
 
 
 def parserequest(request):
-    """At this point we assume the request is a dictionnary from a json file"""
+    """At this point we assume the request is a dictionary from a json file"""
+
     def get_model(response):
+        """Returns a json file with all the Members, Events and Task descriptions"""
         task_list = Task.objects.all()
         event_list = Event.objects.all()
         member_list = Member.objects.all()
         task_elements = []
         event_elements = []
         member_elements = []
-        for task, event, member in zip(task_list, member_list, event_list):
+        for task in task_list:
             task_dictionary = {"id": str(task.id), "event_id": str(task.event_id), "name": str(task.name),
                                "description": str(task.description), "deadline": str(task.deadline),
-                               "responsibles_ids": str(task.responsibles_ids), "completed": str(task.completed)}
+                               "responsibles_ids": json.loads(str(task.responsibles_ids)), "completed": str(task.completed)}
             task_elements.append(task_dictionary)
         response["tasks"] = task_elements
+        for event in event_list:
+            event_dictionary = {"id": str(event.id), "name": str(event.name), "description": str(event.description),
+                                "date_begin": str(event.date_begin), "date_end": str(event.date_end),
+                                "main_organiser_id": str(event.main_organiser_id), "admins_ids": json.loads(event.admins_ids),
+                                "tasks_ids": json.loads(event.tasks_ids), "orgasiners_ids": json.loads(event.organisers_ids)}
+            event_elements.append(event_dictionary)
+        response["events"] = event_elements
+        for member in member_list:
+            member_dictionary = {"id": str(member.id), "name": str(member.name),
+                                 "responsibility": str(member.responsibility),
+                                 "administator": str(member.administrator)}
+            member_elements.append(member_dictionary)
+        response["members"] = member_elements
         return HttpResponse(json.dumps(response))
+
+    def get_authentication(request, response):
+        """Checks if the username and the password is good and sets a new token to the user"""
+        try:
+            user_id = request["id"]
+            user_pass = request["password"]
+            user = Member.objects.get(id=user_id)
+            if user.hashed_password.upper() == hashlib.sha256(user_pass.encode()).hexdigest().upper():
+                generated_token = generate_token(user)
+                response["token"] = generated_token
+                return HttpResponse(json.dumps(response))
+        except (KeyError, Member.DoesNotExist):
+            return HttpResponse(json.dumps({"request_status": "wrong"}))  # Override the response
 
     request_type = request["request_type"]
     response = {}
+    if request_type != "get_authentication_key":
+        try:
+            Member.objects.get(auth_key=request["auth_key"])
+        except Member.DoesNotExist:
+            return HttpResponse(json.dumps({"request_status": "wrong_auth"}))
+        except KeyError:
+            return HttpResponse(json.dumps({"request_status": "no_auth"}))
     if request_type == "get_model":
         response["request_status"] = "ok"
         return get_model(response)
+    elif request_type == "get_authentication_key":
+        return get_authentication(request, response)
 
